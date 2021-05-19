adef7ffb52c9ed3bfeb63496b941d17f1d41d74d
Redlnn <w731347477@gmail.com>
2021-05-19 18:31:34 +0800

    1.5:部分重写，部分配置文件选项更名

--- src/__main__.py
+++ src/__main__.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import subprocess
+
 import sys
-from urllib.request import urlopen
-from os import path, system
+from json import loads as json_loads
+from os import path
 from platform import system as os_type
 from platform import win32_ver
 from tkinter import messagebox, Tk
+from urllib.request import urlopen
 
+from requests import post
 from win10toast import ToastNotifier
 from win32api import GetLastError
 from win32event import CreateMutex
 from winerror import ERROR_ALREADY_EXISTS
 
 import config
-import network_manager
 
 # 使 pyinstaller 能正确导入ico文件
 if getattr(sys, 'frozen', None):
@@ -24,16 +25,48 @@ else:
 
 win = Tk()
 win.withdraw()  # 禁用tkinter主窗口
-toast = ToastNotifier()
-windows_ver = win32_ver()
-
-# 如果正确导入了ico文件则设置tkinter对话框与win10 toast的图标
+# 设置tkinter对话框与win10 toast的图标
 if path.exists(path.join(basedir, 'wangluo.ico')):
     win.iconbitmap(path.join(basedir, 'wangluo.ico'))
     icon_path = path.join(basedir, 'wangluo.ico')
 
+os_type = os_type()
+if os_type == 'Windows':
+    toast = ToastNotifier()
+    windows_ver = win32_ver()
+    is_win = True
+    is_win10 = True if windows_ver[0] == '10' else False  # 如果windows版本等于10则is_win10为True，否则为False
+else:
+    is_win = False
+    is_win10 = False
 
-def connect(host='https://www.baidu.com', timeout=0.5) -> bool:
+cfg = config.read_cfg()  # 读取配置文件
+
+
+def showinfo(title='Ruijie ePorta Tool', msg='test', wait_time=5):
+    """
+    发送tkinter通知框或win10的toast
+
+    :param title: 通知标题
+    :param msg: 通知内容
+    :param wait_time: toast的展示时长
+    """
+
+    if is_win and is_win10:
+        toast.show_toast(title=title, msg=msg, icon_path=icon_path, duration=wait_time, threaded=True)
+    else:
+        messagebox.showinfo(title=title, message=msg)
+
+
+def is_connected(host='https://www.baidu.com', timeout=0.5) -> bool:
+    """
+    测试网络通断状态
+
+    :param host: 用于测试连接状态的地址（带 `http://` 或 `https://` 前缀）
+    :param timeout: 超时时间（单位：秒）
+    :return: 布尔值，True 为通，False 为不通
+    """
+
     try:
         urlopen(host, timeout=timeout)  # Python 3.x
         return True
@@ -41,93 +74,113 @@ def connect(host='https://www.baidu.com', timeout=0.5) -> bool:
         return False
 
 
-# 目前未联网，尝试联网
-def disconnected(system_type):
-    # 目前状态：未联网
-    status = network_manager.connect_network()
-    if status['result'] == 'success' and status['message'] == '':
-        if system_type == 'Windows' and windows_ver[0] == '10':
-            toast.show_toast(title='联网成功', msg='网络已连接！', icon_path=icon_path, duration=6, threaded=True)
-        else:
-            messagebox.showinfo(title='网络已连接', message='联网成功！')
-    elif status['result'] == 'success' and status['message'] != '':
-        if system_type == 'Windows' and windows_ver[0] == '10':
-            toast.show_toast(title='联网成功', msg=status['message'], icon_path=icon_path, duration=10, threaded=True)
-        else:
-            messagebox.showinfo(title='网络已连接', message=status['message'])
-    elif status['result'] == 'fail':
-        error_message1 = '联网失败！\n失败原因：' + status['message']
-        messagebox.showerror(title='联网失败', message=error_message1)
-    elif status['result'] != 'success' and status['result'] != 'fail':
-        error_message2 = '未知错误：' + str(status)
-        messagebox.showerror(title='错误', message=error_message2)
-    # print('返回值：', status)
-    sys.exit(0)
+def connected():
+    """
+    当目前已联网时要执行的操作
 
+    获取配置文件中填写好的header、data、cookie并发送至登录服务器，然后从服务器返回的json文本中判断结果
+    """
 
-# 目前已联网，询问是否需要断网
-def connected(system_type):
-    # 目前状态：已联网
-    # 询问是否需要断网
-    yesno1 = messagebox.askyesno(title='网络已连接', message='设备目前已联网，是否需要断网？', icon='info')
-    if yesno1:
+    # 询问用户是否需要断网
+    if messagebox.askyesno(title='网络已连接', message='设备目前已联网，是否需要断网？', icon='info'):
         # 用户选择断网，尝试断网
-        status = network_manager.disconnect_network()
-        if status['result'] == 'success':
-            if system_type == 'Windows' and windows_ver[0] == '10':
-                toast.show_toast(title='已断网', msg='断网成功！', icon_path=icon_path, duration=5)
-            else:
-                messagebox.showinfo(title='已断网', message='断网成功！')
-        elif status['result'] == 'fail':
-            error_message1 = '断网失败！\n失败原因：' + status['message']
-            messagebox.showerror(title='断网失败', message=error_message1)
+        url = cfg['server']['logout_url']
+        data = cfg['logout_data']
+        header = cfg['logout_header']
+        cookie = cfg['cookie']
+
+        try:
+            res = post(url=url, data=data, headers=header, cookies=cookie)
+        except Exception as e:
+            error_message = f'出现错误！\n\n错误信息：{str(e)}'
+            messagebox.showerror(title='未知错误', message=error_message)
+            sys.exit(1)
         else:
-            error_message2 = '未知错误：' + str(status)
-            messagebox.showerror(title='错误', message=error_message2)
-        # print('返回值：', status)
-    # 用户选择不断网，程序自动退出
+            res.encoding = 'utf-8'
+            status = json_loads(res.text)
+            if status['result'] == 'success':
+                showinfo(title='已断网', msg='断网成功！', wait_time=5)
+            elif status['result'] == 'fail':
+                error_message = f'断网失败！\n失败原因：{status["message"]}'
+                messagebox.showerror(title='断网失败', message=error_message)
+            else:
+                error_message = f'未知错误：{str(status)}'
+                messagebox.showerror(title='错误', message=error_message)
+            print('服务器返回内容：', status)
     sys.exit(0)
 
 
-def main(system_type):
+def disconnected():
+    """
+    当目前没有联网时要执行的操作
+
+    获取配置文件中填写好的header、data、cookie并发送至登录服务器，然后从服务器返回的json文本中判断结果
+    """
+
+    url = cfg['server']['login_url']
+    data = cfg['login_data']
+    header = cfg['login_header']
+
+    try:
+        res = post(url=url, data=data, headers=header)
+    except Exception as e:
+        error_message = f'出现错误！\n\n错误信息：{str(e)}'
+        messagebox.showerror(title='未知错误', message=error_message)
+        sys.exit(1)
+    else:
+        res.encoding = 'utf-8'
+        status = json_loads(res.text)
+        if status['result'] == 'success' and status['message'] == '':
+            showinfo(title='联网成功', msg='网络已连接！', wait_time=6)
+        elif status['result'] == 'success' and status['message'] != '':
+            showinfo(title='联网成功', msg=status['message'], wait_time=6)
+        elif status['result'] == 'fail':
+            error_message = '联网失败！\n失败原因：' + status['message']
+            messagebox.showerror(title='联网失败', message=error_message)
+        elif status['result'] != 'success' and status['result'] != 'fail':
+            error_message = '未知错误：' + str(status)
+            messagebox.showerror(title='错误', message=error_message)
+        print('服务器返回内容：', status)
+    sys.exit(0)
+
+
+def main():
+    """
+    主函数，判断网络通断情况并执行相应操作
+    """
+
     # 先判断网络通断再执行操作
-    # noinspection PyBroadException
-    f = config.read_json()
-    if f['config']['enable_disconnect']:
-        if connect():
-            connected(system_type)
+    if cfg['config']['enable_network_status_check']:  # 读配置文件，是否需要判断网络通断
+        if is_connected():  # 判断网络通断
+            connected()
         else:
-            if f['config']['allow_ping']:
-                if not connect(host=f'http://{f["config"]["server"]}', timeout=0.3):
-                    if system_type == 'Windows' and windows_ver[0] == '10':
-                        toast.show_toast(title='网络环境错误', msg='当前不在校园网环境，不自动尝试连接校园网')
-                    else:
-                        messagebox.showinfo(title='网络环境错误', message='当前不在校园网环境，不自动尝试连接校园网')
+            if cfg['config']['enable_school_network_check']:
+                if not is_connected(host=cfg["config"]["server_url"], timeout=0.3):  # 判断是否能连接到登录服务器
+                    # 如果无法连接到登录服务器则发送通知并退出
+                    showinfo(title='网络环境错误', msg='当前不在校园网环境，不自动尝试连接校园网')
                     sys.exit(0)
                 else:
-                    disconnected(system_type)
+                    disconnected()
             else:
-                disconnected(system_type)
+                disconnected()  # 如果不判断是否能连接到登录服务器，则
     else:
-        # 配置文件未启用"enable_disconnect"，程序将始终当作设备未联网
-        disconnected(system_type)
+        disconnected()  # 配置文件未启用"enable_network_status_check"，程序将始终当作设备未联网
 
 
 if __name__ == '__main__':
     # 判断系统类型
     try:
-        sys_type = os_type()
-        if sys_type == 'Windows':
+        if is_win:
             # 检测多开
-            mutex_name = 'school_network'
+            mutex_name = 'ruijie_eporta_tool'  # noqa
             mutex = CreateMutex(None, False, mutex_name)
             if GetLastError() == ERROR_ALREADY_EXISTS:
                 messagebox.showerror(title='警告', message='本程序不能同时运行多个实例，请不要多开。\n若右下角仍有本程序通知，请关掉通知后重试！')
                 sys.exit(0)
             else:
-                main(sys_type)
+                main()
         else:
             # 系统不是Windows，直接启动（没有Linux环境也懒得搞）
-            main(sys_type)
+            main()
     except KeyboardInterrupt:
         sys.exit()
--- src/config.py
+++ src/config.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import subprocess
 import sys
 from json import dump as json_dump
 from json import load as json_load
-from os import path, system
+from os import path
 from tkinter import messagebox, Tk
 
 # 使编译器能正确导入ico文件
@@ -19,10 +18,12 @@ win.withdraw()  # 禁用tkinter主窗口
 if path.exists(path.join(basedir, 'wangluo.ico')):
     win.iconbitmap(path.join(basedir, 'wangluo.ico'))
 
+current_config_version = 2
+
 
 # 写入配置文件
 def write_json():
-    warn = {
+    tips = {
         '警告1': '除非抓到的包中有更多的参数，否则请不要增删本配置文件中的任何条目或改动顺序',
         '提示1': '本配置文件内容可能需要根据学校服务器设置及时更新',
         '提示2': '以下为各大项配置的用途解释',
@@ -36,17 +37,16 @@ def write_json():
     }
     # 程序设置
     config = {
-        '请不要随意修改config_version的值': '',
-        'config_version': 1,
-        'allow_ping 指校园网登录服务器是否允许ping': '根据下面server项的值ping，若关闭则不检测是否为校园网环境',
-        '请填写校园网登录页的地址，不带http://或https://，结尾也不要有斜杠': '只填写ip或xx.xxx.com格式即可',
-        'allow_ping': True,
-        'server': '127.0.0.1',
-        'enable_disconnect 用于控制本程序是否判断网络通断以及在网络已连接时运行本程序是否弹出断网对话框': '',
-        '关闭后本程序将不再判断网络状况，始终当作设备未联网': '关闭本选项以及上方的 allow_ping 后将不会弹出 ping 命令的 CMD 窗口',
-        'enable_disconnect': True
+        '重要提醒': '请不要随意修改config_version的值！',
+        'config_version': current_config_version,
+        '选项解释-enable_school_network_check': '指是否需要判断当前网络环境是否为校园网环境，若设置为false则不检测',
+        'enable_school_network_check': True,
+        '选项解释-server_url': '校园网登录服务器的地址用于判断当前网络环境是否为校园网环境',
+        'server_url': 'http://127.0.0.1',
+        '选项解释-enable_network_status_check': '是否判断网络通断，开启后在网络已连接时运行本程序将弹出是否需要断网的对话框',
+        'enable_network_status_check': True
     }
-    # 登录与登出所使用目标服务器地址
+    # 上下线时所连接的服务器的地址
     server = {
         'login_url': 'http://127.0.0.1/eportal/InterFace.do?method=login',
         'logout_url': 'http://127.0.0.1/eportal/InterFace.do?method=logout',
@@ -71,7 +71,7 @@ def write_json():
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'Connection': 'keep-alive',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/85.0.4183.102 Safari/537.36 Edg/85.0.564.51',
+                      'Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.62 Edge/18.19041',
         'Accept': '*/*',
         'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
         'Accept-Encoding': 'gzip, deflate',
@@ -83,7 +83,7 @@ def write_json():
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'Connection': 'keep-alive',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/85.0.4183.102 Safari/537.36 Edg/85.0.564.51',
+                      'Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.62 Edge/18.19041',
         'Accept': '*/*',
         'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
         'Accept-Encoding': 'gzip, deflate',
@@ -106,30 +106,28 @@ def write_json():
 
     # 构造json文本
     all_json = {
-        'warn': warn,
+        'tips': tips,
         'config': config,
         'server': server,
-        'login_data': login_data,
-        'logout_data': logout_data,
-        'login_header': login_header,
-        'logout_header': logout_header,
         'cookie': cookie,
+        'login_data': login_data,
+        'login_header': login_header,
+        'logout_data': logout_data,
+        'logout_header': logout_header,
     }
 
-    # noinspection PyBroadException
     try:
-        with open('./config.json', 'w', encoding='utf-8') as f:
+        with open('config.json', 'w', encoding='utf-8') as f:
             json_dump(all_json, f, ensure_ascii=False, indent=4)
-        f.close()
     except Exception as e:
-        error_message = '尝试写入新配置文件时出错\n' + str(e)
+        error_message = f'尝试写入新配置文件时出错\n{str(e)}'
         messagebox.showerror(title='错误', message=error_message)
         sys.exit(1)
 
 
-def read_json():
+def read_cfg():
     try:
-        f = open('./config.json', mode='r', encoding='utf-8')
+        f = open('config.json', mode='r', encoding='utf-8')
     except IOError:
         write_json()
         messagebox.showwarning(title='警告', message='配置文件不存在，已生成新配置文件，请填写配置文件后重试!')
@@ -137,18 +135,20 @@ def read_json():
     else:
         config = json_load(f)  # 读取配置文件
         f.close()
-        # 如果配置文件版本不存在或版本低于1
-        if not config['config']['config_version'] or config['config']['config_version'] < 1:
+        # 检测配置文件版本
+        if not config['config']['config_version']:
             messagebox.showerror(title='警告', message='读取配置文件时出错，原因：\n\n    配置文件格式过期，请备份并删除原配置文件后运行本程序重新生成！')
             sys.exit(1)
-        # 如果配置文件版本存在且版本大于1
-        elif config['config']['config_version'] and config['config']['config_version'] > 1:
-            messagebox.showerror(title='警告', message='读取配置文件时出错，原因：\n\n    配置文件版本过高，请更新本程序。\n'
-                                                     '    如您使用的程序已是最新，请勿更改配置文件中"config_version"的值')
-            sys.exit(1)
+        else:
+            if config['config']['config_version'] < current_config_version:
+                messagebox.showerror(title='警告', message='读取配置文件时出错，原因：\n\n    配置文件格式过期，请备份并删除原配置文件后运行本程序重新生成！')
+                sys.exit(1)
+            elif config['config']['config_version'] > current_config_version:
+                messagebox.showerror(title='警告', message='读取配置文件时出错，原因：\n\n    配置文件版本过高，请更新本程序。\n'
+                                                         '    如您使用的程序已是最新，请勿更改配置文件中"config_version"的值')
+                sys.exit(1)
 
-        if config['server']['login_url'] == 'http://127.0.0.1/eportal/InterFace.do?method=login' \
-                or config['server']['logout_url'] == 'http://127.0.0.1/eportal/InterFace.do?method=logout':
+        if config['login_data']['userId'] == '00000000000':
             messagebox.showwarning(title='警告', message='配置文件未正确填写，请填写配置文件后重试!')
             sys.exit(1)
         return config
